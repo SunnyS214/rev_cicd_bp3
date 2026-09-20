bd2613ff081cba0b5f4a89d4f98c0eb3bc3c763d
Sunny <sunny11@mail.com>
2026-09-20 08:27:08 +0000

    github.sha added in pipeline 1.1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express= require('express')
 const app= express()
 const port =  3000
 app.get("/" ,(req , res)=>{
-    res.send("docker build push cicd pipeline bp3")
+    res.send("docker build push cicd pipeline bp3 latest ${{github.sha}}")
 } )
 
 app.listen(port , ()=>{
